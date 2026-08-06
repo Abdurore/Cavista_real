88573cf14951b167b9164a83c003212b3aac4168
Abdulhameed Oluwadurotimi Oreagba <137651522+Abdurore@users.noreply.github.com>
2026-08-06 23:02:24 +0100

    Add new allowed origin for CORS

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://cavista-real-w2eb.vercel.app",
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
